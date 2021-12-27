2f7d05abccdc3a7b2dd7f277b40aabc645ea5261
vlad4k5 <armia1823@gmail.com>
2021-12-27 21:25:20 +0200

    add eslint and prettier to project

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,16 +1,21 @@
-import s from "./ProductItem.module.scss"
+import s from './ProductItem.module.scss'
+import React from 'react'
 
-const ProductItem = ({img, link, name, price, category}) => {
-  return <a className={s.productItem} href={link}>
-    <div className={s.topSide}>
-      <img className={s.productImg} src={img} width={220} alt="Product" />
-      <div className={s.productName}>{name.length > 50 ? name.slice(0,50) + "..." : name}</div>
-    </div>
-    
-    <div className={s.bottomSide}>
-      <div className={s.productCategory}>{category}</div>
-      <div className={s.productPrice}><span>Price:</span> {price}$</div>
-    </div>
-  </a>
+const ProductItem = ({ img, link, name, price, category }) => {
+  return (
+    <a className={s.productItem} href={link}>
+      <div className={s.topSide}>
+        <img className={s.productImg} src={img} width={220} alt="Product" />
+        <div className={s.productName}>{name.length > 50 ? name.slice(0, 50) + '...' : name}</div>
+      </div>
+
+      <div className={s.bottomSide}>
+        <div className={s.productCategory}>{category}</div>
+        <div className={s.productPrice}>
+          <span>Price:</span> {price}$
+        </div>
+      </div>
+    </a>
+  )
 }
 export default ProductItem
